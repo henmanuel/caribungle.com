3f61b18ca4e3123be2392942441ac60cb48a347b
henmanuel <henmanuelvargas@gmail.com>
2023-05-30 12:59:56 -0600

    Start

---
+++ src/precache-manifest.2c0498a2e0a8eae0fe526f1753c8ca27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6988f80574db15d0f6f59052e4b9f60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adbb2aedbdac405868a9",
+    "url": "/static/js/2.7d653166.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.7d653166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cb352b46117dff8c373",
+    "url": "/static/js/main.e1dc0f9e.chunk.js"
+  },
+  {
+    "revision": "b79fab24fc06a34fadb3",
+    "url": "/static/js/runtime-main.c41a6ea3.js"
+  }
+]);
